206cc2cfd0f549d898b591b3593fb75457e3e364
Jendrik Johannes <jendrik@onepiece.software>
2026-01-30 13:28:20 +0100

    Update org.gradlex.internal-build-conventions to v0.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,9 +4,6 @@ version = "0.10"
 
 tasks.compileJava { options.release.set(17) }
 
-// TODO remove with next internal-build-conventions update
-spotless.java { targetExclude("build/**") }
-
 dependencies {
     implementation("com.diffplug.spotless:spotless-plugin-gradle:8.2.1") {
         // Exclude transitive dependencies of JGit as we do not need git functionality.
